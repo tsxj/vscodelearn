978e4202c69b4f5e49430cdf8ebfeb8078e55bc5
tsxj <1908854252@qq.com>
2020-11-20 09:10:40 +0800

    git push

--- test_compile_to_multifile/main.c
+++ test_compile_to_multifile/main.c
@@ -2,7 +2,7 @@
 #include "include/output.h"
 int main()
 {
-   printf("helloworld");
+   printf("helloworld"); //提示
    printf("hellochina");
    inout();
     return 0;
--- test_gdb/main.cpp
+++ test_gdb/main.cpp
@@ -4,14 +4,15 @@ using   namespace std;
 int main(int argc, char** argv)
 {
 
-     // cout<<"this is a test"<<endl;
+      cout<<"this is a test"<<endl;
       int N=100;
       int sum=0;
       int i=1;
       while(i<N)
       {
-          sum+=i;
+          sum+=i; //this
           i=i+1;
+          cout<<"zonghe="<<sum<<endl;
       }
       cout<<"sum="<<sum<<endl;
       cout<<"the program is over!"<<endl;
