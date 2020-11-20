c8e184b487ab352fc68c52547c955bfe461034bf
tsxj <1908854252@qq.com>
2020-11-20 21:50:35 +0800

    vscodelearn_devtijiao

---
+++ arm-linux-gcc/newproject/build/CMakeFiles/helloworld_cmake_arm_exe.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wsx/work/test/vscodelearn/arm-linux-gcc/newproject/main.c" "/home/wsx/work/test/vscodelearn/arm-linux-gcc/newproject/build/CMakeFiles/helloworld_cmake_arm_exe.dir/main.c.o"
+  "/home/wsx/work/test/vscodelearn/arm-linux-gcc/newproject/src/Gun.c" "/home/wsx/work/test/vscodelearn/arm-linux-gcc/newproject/build/CMakeFiles/helloworld_cmake_arm_exe.dir/src/Gun.c.o"
+  "/home/wsx/work/test/vscodelearn/arm-linux-gcc/newproject/src/Soilger.c" "/home/wsx/work/test/vscodelearn/arm-linux-gcc/newproject/build/CMakeFiles/helloworld_cmake_arm_exe.dir/src/Soilger.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ arm-linux-gcc/newproject/build/CMakeFiles/helloworld_cmake_arm_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld_cmake_arm_exe.dir/main.c.o"
+  "CMakeFiles/helloworld_cmake_arm_exe.dir/src/Gun.c.o"
+  "CMakeFiles/helloworld_cmake_arm_exe.dir/src/Soilger.c.o"
+  "helloworld_cmake_arm_exe.pdb"
+  "helloworld_cmake_arm_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/helloworld_cmake_arm_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
