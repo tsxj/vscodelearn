8afea5bad50a660d199e719272e80cbe030f3ba0
tsxj <1908854252@qq.com>
2020-11-21 16:51:46 +0800

    vscodelearn_devtijiao

--- c_make/cmakelearn0/main.cpp
+++ c_make/cmakelearn0/main.cpp
@@ -1,8 +1,7 @@
-#include<iostream>
-using namespace std;
+#include "Gun.h"
 int main()
 {
-   
+   prino();
    cout<<"this is a test!\n"<<endl;
    cout<<"helloworld\n"<<endl;
     return 0;
---
+++ c_make/cmakelearn0/src/Gun.cpp
@@ -0,0 +1,7 @@
+
+#include"Gun.h"
+int prino()
+{
+    cout<<"this is from Gun.cpp"<<endl;
+    return 0;
+}
